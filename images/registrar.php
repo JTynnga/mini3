12e3c62dbe3ade1713bf4fe0a5b1b3aaa07873f9
Jose Teodoro Ynga Chiroque <jose.ynga@gmail.com>
2023-07-24 15:42:04 -0500

    second commit

---
+++ images/registrar.php
@@ -0,0 +1,126 @@
+<?php
+session_start();
+if (!isset($_SESSION["datos"])) {
+    echo "USUARIO NO REGISTRADO !";
+    die();
+} ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link href="styles.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <section id="container">
+        <img class="imgChallen" src="./images/devchallenges.svg" alt="devchallenges.svg" /><br><br>
+        <nav class="nav-i">
+            <div class="img-ch">
+                <img src="./images/devchallenges.svg" alt="devchallenges.svg" />
+            </div>
+            <div id="open" class=" open-i">
+                <div class="open-ii">
+                    <?php
+                    echo "<img class='img-i' src='data:image/jpg; base64," . base64_encode($_SESSION["dato_imagen"]) . "'>"; ?>
+                </div>
+                <p class="collapse"><?php echo $_SESSION["dato_nombre"] ?></p>
+                <svg id="spin" class=" collapse1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="-8 0 30 10">
+                    <path d="M15.434 1.235A2 2 0 0 0 13.586 0H2.414A2 2 0 0 0 1 3.414L6.586 9a2 2 0 0 0 2.828 0L15 3.414a2 2 0 0 0 .434-2.179Z" />
+                </svg>
+
+            </div>
+            <ul id="toggle">
+                <div class="infop">
+                    <a href="registrar.php">
+                        <svg class="svg-i" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 20 20">
+                            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                        </svg>
+                        <li class="profile">My
+                            Profile</li>
+                    </a>
+                </div>
+
+                <div class="info-i">
+                    <a href="registrar.php" class="registrar>
+                            <svg class=" icon-r" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 20 18">
+                        <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                        </svg>
+                        <li class="group">
+                            Group Chat
+                        </li>
+                    </a>
+                </div>
+
+                <div class="logout">
+                    <a href="logout.php">
+                        <svg class="svg-ii" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+                            <path stroke="red" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
+                        </svg>
+                        <li class="li-i">
+                            Log out
+                        </li>
+                    </a>
+                </div>
+
+            </ul>
+        </nav>
+
+
+
+
+        <h2>Join thousands of learners from around the world</h2><br>
+        <h3>Master web development by making real-life projects. There are multiple paths for
+            you to choose</h3><br><br>
+
+        <div id="email">
+            <img class="imgEmail" src="./images/email.svg" alt="email.svg" />
+            <input class="inpEmail" type="text" name="email" placeholder="Email" required />
+        </div><br><br>
+
+        <div id="passw">
+            <img class="imgPassw" src="./images/password.svg" alt="passw.svg" />
+            <input class="inpPassw" type="password" name="passw" placeholder="Password" required />
+        </div><br><br>
+
+        <p>
+            <!--&#160;-->
+            <?php
+            if (isset($_SESSION["errorx"])) {
+                echo $_SESSION["errorx"];
+            };
+            unset($_SESSION["errorx"]);
+            ?>
+        </p>
+        <button class="botonEnv" type="submit" name="start">Star coding now</button><br><br><br>
+
+        <div id="continue">
+            <p>or continue with these social profile,</p>
+        </div><br><br>
+
+        <div class="botones">
+            <a href="#"><img src="./images/Google.svg" alt="google.svg" /></a>
+            <a href="#"><img src="./images/Facebook.svg" alt="Facebook.svg" /></a>
+            <a href="#"><img src="./images/Twitter.svg" alt="Twitter.svg" /></a>
+            <a href="#"><img src="./images/Gihub.svg" alt="Gihub.svg" /></a>
+        </div><br><br>
+        <div class="member">
+            <p>Already a member? </p>
+            <p><a href="./login.php">&#160;Login</a></p>
+        </div>
+        </form>
+        <div class="footer">
+            <p>create by <u><a href="https://github.com/joseynga"> JoseYnga </a></u></p>
+            <p>devChallenges.io</p>
+        </div>
+    </section>
+
+</body>
+
+</html>
--- methods.php
+++ methods.php
@@ -1,4 +1,65 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    session_start();
+    $_SESSION["email"] = $_POST["correo"];
+    $_SESSION["passw"] = $_POST["clave"];
+
+    $email_u = $_SESSION["email"];
+    $passw_u = $_SESSION["passw"];
+
+    require("connection.php");
+    $hash = password_hash($pass_u, PASSWORD_DEFAULT);
+
+    $repeEmail = "SELECT * FROM usuarios WHERE email = '$email_u'";
+
+    $resultTryLg = $mysqli->query($repeEmail);
+    $datosTryLg = $resultTryLg->fetch_assoc();
+    $datosTryLgPass = $datosTryLg["clave"];
+
+    if (password_verify($passw_u, $datosTryLgPass)) {
+        $_SESSION["error_x"] = "Are you trying to login?";
+        header("Location: index.php");
+        die();
+    } else {
+        $trySame = $mysqli->query($repeEmail);
+
+        $numFilasS = $trySame->num_rows;
+
+        if ($numFilasS === 1) {
+            $_SESSION["error_x"] = "This account already exist.";
+            header("Location: index.php");
+            die();
+        } else {
+            $insertarUsers_Info = "INSERT INTO usuarios (correo, clave) VALUES
+            ('$email_u','$hash');";
+
+            $ejecutarDatos = $mysqli->query($insertarUsers_Info);
+
+            $showInfo = "SELECT * FROM users_info WHERE email = '$email_u' AND password = '$hash';";
+
+            $resultado = $mysqli->query($showInfo);
+
+            $datos = $resultado->fetch_assoc();
+            $_SESSION["datos"] = $datos;
+
+            $_SESSION["dato_id"] = $_SESSION["datos"]["id"];
+            $_SESSION["dato_imagem"] = $_SESSION["datos"]["imagen"];
+            $_SESSION["dato_nombre"] = $_SESSION["datos"]["nombre"];
+            $_SESSION["dato_bio"] = $_SESSION["datos"]["bio"];
+            $_SESSION["dato_telef"] = $_SESSION["datos"]["telef"];
+            $_SESSION["dato_email"] = $_SESSION["datos"]["correo"];
+            $_SESSION["dato_passw"] = $_SESSION["datos"]["clave"];
+
+            header("Location: registrar.php");
+        }
+    }
+}
+
+
+
+
+/*<!-- 
+?php
 require("connection.php");
 $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
@@ -15,3 +76,4 @@ $query = "INSERT INTO usuarios (nombre,correo,clave,imagen) VALUES ('$nombre','$
 
 $resultado = $mysqli->query($query);
 print_r($resultado);
+-->*/
